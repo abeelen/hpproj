0addb5e65975a53159b4c5c2d9e414f143d91c59
abeelen <alexandre.beelen@ias.u-psud.fr>
2016-02-12 10:46:33 +0100

    Generalized ini file/path

--- cross_match.py
+++ cross_match.py
@@ -9,343 +9,141 @@ from astropy.io import ascii
 import csv
 from astropy import wcs
 from astropy import coordinates as coord
-import config_pact
+import configparser
 from astropy.table import vstack as astvstack
 from astropy.table import hstack as asthstack
 from astropy.table import Table, join
-import os 
+import os
 #from astroquery.ned import Ned
 
 
 
-def cross_match( file_input,glonglat=[0.,0.],coordframe='galactic'):
+def cross_match( file_input,lonlat=[6.7,30.45],coordframe='galactic'):
 
     """ can be run doing
     import cross_match as cm
     tab = cm.main( "cross_match_TOTO.ini")
     """
 
-    if file_input == []:
+    if not file_input:
         print('------------------------------')
         print('Need an input file as argument')
         print('------------------------------')
         return
 
-    
-
-####INIs
-    
     #reads the ini file
 
-    conf = config_pact.config_pact(input_file=file_input)
+    conf = configparser.ConfigParser(interpolation=configparser.ExtendedInterpolation())
+    conf.read(file_input)
 
 
     # CATALOG INPUT to be checked with the ones in .ini
     # TODO: should be given as argument
 
-
-
-    if conf.input_cat:
+    if conf['inputs']['cat']:
 	#cat=fits.open('PACT_cat_equ_reg1.fits')
-        cat    = fits.open(conf.input_cat)
-        cc     = cat[1].data
+        cc    = fits.getdata(conf.get('inputs','cat'), conf.getint('inputs','ext'))
         cgla   = cc['glat'].squeeze()
         cglo   = cc['glon'].squeeze()
         index  = np.arange(np.size(cgla))
-        pacts1 = coord.SkyCoord(cglo,cgla, unit=u.deg, frame='galactic')
+        coord_in = coord.SkyCoord(cglo,cgla, unit=u.deg, frame='galactic')
     else:
-        cglo   = [glonglat[0]]
-        cgla   = [glonglat[1]]
+        cglo   = [lonlat[0]]
+        cgla   = [lonlat[1]]
         index  = np.arange(np.size(cgla))
-        pacts1 = coord.SkyCoord(cglo,cgla, unit=u.deg, frame=coordframe)
-        #print('')
-        #print('Checking object: ', pacts1)
-        #print('')
-    
+        coord_in = coord.SkyCoord(cglo,cgla, unit=u.deg, frame=coordframe)
+
     tab_input_cat = Table()
     tab_input_cat.add_column(Table.Column(name='INDEX', data=index))
     tab_input_cat.add_column(Table.Column(name='GLON',  data=cglo))
     tab_input_cat.add_column(Table.Column(name='GLAT',  data=cgla))
-    
+
     new = tab_input_cat
 
-    
-#####SZDB
+    match_result = {}
 
-    if (conf.szdb):
-        
-        catsz      = fits.open(conf.szdb)
-        ccsz       = catsz[1].data
-        coord_szdb = coord.SkyCoord(ccsz['glon'],ccsz['glat'], unit=u.deg, frame='galactic')
+    for cat in conf.get('global','cat_list').split():
+        if conf.has_section(cat):
+            lon = conf.get(cat,'lon')
+            lat = conf.get(cat,'lat')
+            frame = conf.get(cat,'frame')
+            fields = conf.get(cat,'fields').split()
 
-        # cross matching
-        temp_id_szdb, temp_sep_szdb, trash=pacts1.match_to_catalog_sky(coord_szdb)
+            dist_threshold  = conf.getfloat(cat, 'dist_threshold_arcmin')
 
-        temp_id_szdb  = temp_id_szdb.squeeze()
-        
+            cc = fits.getdata(conf.get(cat,'file'), conf.getint(cat,'ext'))
+            coord_cc = coord.SkyCoord(cc[lon], cc[lat], unit=u.deg, frame=frame)
 
-        # selecting associations with distance <  sz_dist_theshold_arcmin
-        wheregood = temp_sep_szdb.arcmin.squeeze() < conf.sz_dist_theshold_arcmin
-        #defining new columns related to the cross match
+            temp_id, temp_sep, trash=coord_in.match_to_catalog_sky(coord_cc)
 
-        if np.sum(wheregood)>0:
-            if np.sum(wheregood)>1:
-                x_id_szdb_in_cat = np.arange(np.size(cgla))[wheregood]
-                x_id_szdb        = temp_id_szdb[wheregood]
-                x_coord_szdb     = coord_szdb[x_id_szdb]
-                x_sep_szdb       = (temp_sep_szdb.squeeze())[wheregood]
-            elif np.sum(wheregood)==1:
-                x_id_szdb_in_cat = [0]
-                x_id_szdb        = [temp_id_szdb*1]
-                x_coord_szdb     = coord_szdb[x_id_szdb]
-                x_sep_szdb       = temp_sep_szdb
-            
-            
+            # selecting associations with distance <  sz_dist_theshold_arcmin
+            wheregood = temp_sep.arcmin.squeeze() < dist_threshold
+            #defining new columns related to the cross match
 
-        # building infos for selected fields (sz_fields)
-        #xszdb = ccsz.take(x_id_szdb)
-        
-        #xszdb =Table()
-        #for fi in conf.sz_fields:
-        #    xszdb.add_column(Table.Column(name=fi, data=ccsz[fi][x_id_szdb]))
-#ou
-        
-            fields = [str(x) for x in conf.sz_fields] # removes u
-            xszdb  = Table(ccsz[x_id_szdb])           # make cat as Table
-            xszdb.keep_columns(fields)                # extract the fields
-
-        # adding columns related to the crossmatch
-            xszdb.add_column(Table.Column(name='INDEX', data=x_id_szdb_in_cat))
-            xszdb.add_column(Table.Column(name='SZDB_ID', data=x_id_szdb))
-            xszdb.add_column(Table.Column(name='SZDB_SEP', data=x_sep_szdb.arcmin))
-                 
-            for fi in conf.sz_fields:
-                xszdb.rename_column(fi, 'SZDB_'+fi)
-
-        
-            new = join(new, xszdb,keys='INDEX', join_type='outer')
-            new['SZDB_ID'].fill_value=-1
-       
-            if (conf.verbose):
-                print('######################################')
-                print('Results of cross correlation with SZDB')
-                print(new)
-        else:
-            xszdb  = Table(masked=True)
-
-            xszdb.add_column(Table.Column(name='INDEX', data=[0]))
-            xszdb.add_column(Table.Column(name='SZDB_ID', data=[-1]))
-            xszdb.add_column(Table.Column(name='SZDB_SEP', data=[-1.0]))
-            new = join(new, xszdb,keys='INDEX', join_type='outer')
-            new['SZDB_ID'].fill_value=-1
-            new['SZDB_ID'].mask=True
-        #xszdb.keep_columns(['RA','DEC']) # becareful remove everything else
-        # save Table or add Table to the others(X, Op)?        
+            if wheregood.any():
+                if np.sum(wheregood)>1:
+                    x_id_in_cat = index[wheregood.squeeze()]
+                    x_id        = temp_id[wheregood]
+                    x_coord     = coord_cc[x_id]
+                    x_sep       = temp_sep[wheregood]
+                elif np.sum(wheregood)==1:
+                    x_id_in_cat = [0]
+                    x_id        = [temp_id*1]
+                    x_coord     = coord_cc[x_id]
+                    x_sep       = temp_sep
 
 
-####PXCC
+                # fields = [str(x) for x in fields] # removes u
+                ccm_tab  = Table(cc[x_id])         # make cat as Table
+                ccm_tab.keep_columns(fields)        # extract the fields
 
-    if (conf.pxcc):
+                for field in fields:
+                    ccm_tab.rename_column(field, cat.upper()+'_'+field)
 
-        catx      = fits.open(conf.pxcc)
-        ccx       = catx[1].data
-        coord_pxcc = coord.SkyCoord(ccx['glon'],ccx['glat'],
-                                    unit=u.deg, frame='galactic')
- 
-        temp_id_pxcc, temp_sep_pxcc, trash = pacts1.match_to_catalog_sky(
-                                             coord_pxcc)
-
-        temp_id_pxcc  = temp_id_pxcc.squeeze()
-
-        wheregood = temp_sep_pxcc.arcmin.squeeze() < conf.x_dist_theshold_arcmin
-
-        if np.sum(wheregood)>0:
-            if np.sum(wheregood)>1:
-                x_id_pxcc_in_cat = np.arange(np.size(cgla))[wheregood]
-                x_id_pxcc        = temp_id_pxcc[wheregood]
-                x_coord_pxcc     = coord_pxcc[x_id_pxcc]
-                x_sep_pxcc       = (temp_sep_pxcc.squeeze())[wheregood]
-            elif np.sum(wheregood)==1:
-                x_id_pxcc_in_cat = [0]
-                x_id_pxcc        = [temp_id_pxcc*1]
-                x_coord_pxcc     = coord_pxcc[x_id_pxcc]
-                x_sep_pxcc       = temp_sep_pxcc
- 
-            fields = [str(x) for x in conf.x_fields] # removes u
-            xpxcc  = Table(ccx[x_id_pxcc])           # make cat as Table
-            xpxcc.keep_columns(fields)                # extract the fields
-
-            xpxcc.add_column(Table.Column(name='INDEX', data=x_id_pxcc_in_cat))
-            xpxcc.add_column(Table.Column(name='PXCC_ID', data=x_id_pxcc))
-            xpxcc.add_column(Table.Column(name='PXCC_SEP', data=x_sep_pxcc.arcmin))
-
-        
-            for fi in conf.x_fields:
-                xpxcc.rename_column(fi, 'PXCC_'+fi)
-                
-            new = join(new, xpxcc,keys='INDEX', join_type='outer')
-            new['PXCC_ID'].fill_value=-1
-       
-            if (conf.verbose):
-                print('######################################')
-                print('Results of cross correlation with PXCC')
-                print(new)
-        else:
-            xpxcc  = Table(masked=True)
-
-            xpxcc.add_column(Table.Column(name='INDEX', data=[0]))
-            xpxcc.add_column(Table.Column(name='PXCC_ID', data=[-1]))
-            xpxcc.add_column(Table.Column(name='PXCC_SEP', data=[-1.0]))
-            new = join(new, xpxcc,keys='INDEX', join_type='outer')
-            new['PXCC_ID'].fill_value=-1
-            new['PXCC_ID'].mask=True
-       
-
-#####REDMAPPER
-            
-    if (conf.rdm):
-
-        catrdm      = fits.open(conf.rdm)
-        ccrdm       = catrdm[1].data
-        coord_rdm = coord.SkyCoord(ccrdm['ra'],ccrdm['dec'],
-                                    unit=u.deg, frame='fk5')
- 
-        temp_id_rdm, temp_sep_rdm, trash = pacts1.match_to_catalog_sky(
-                                             coord_rdm)
-
-        temp_id_rdm  = temp_id_rdm.squeeze()
-
-        wheregood = temp_sep_rdm.arcmin.squeeze() < conf.rdm_dist_theshold_arcmin
-
-        if np.sum(wheregood)>0:
-            if np.sum(wheregood)>1:
-                x_id_rdm_in_cat = np.arange(np.size(cgla))[wheregood]
-                x_id_rdm        = temp_id_rdm[wheregood]
-                x_coord_rdm     = coord_rdm[x_id_rdm]
-                x_sep_rdm       = (temp_sep_rdm.squeeze())[wheregood]
-            elif np.sum(wheregood)==1:
-                x_id_rdm_in_cat = [0]
-                x_id_rdm        = [temp_id_rdm*1]
-                x_coord_rdm     = coord_rdm[x_id_rdm]
-                x_sep_rdm       = temp_sep_rdm
-
-            fields = [str(rdm) for rdm in conf.rdm_fields] # removes u
-            xrdm  = Table(ccrdm[x_id_rdm])           # make cat as Table
-            xrdm.keep_columns(fields)                # erdmtract the fields
-
-            xrdm.add_column(Table.Column(name='INDEX', data=x_id_rdm_in_cat))
-            xrdm.add_column(Table.Column(name='RDM_ID', data=x_id_rdm))
-            xrdm.add_column(Table.Column(name='RDM_SEP', data=x_sep_rdm.arcmin))
-
-            for fi in conf.rdm_fields:
-                xrdm.rename_column(fi, 'RDM_'+fi)
-
-            new = join(new, xrdm,keys='INDEX', join_type='outer')
-            new['RDM_ID'].fill_value=-1
-
-        
-            if (conf.verbose):
-                print('######################################')
-                print('Results of cross correlation with RDM')
-                print(new)
-        else:
-            xrdm  = Table(masked=True)
-
-            xrdm.add_column(Table.Column(name='INDEX', data=[0]))
-            xrdm.add_column(Table.Column(name='RDM_ID', data=[-1]))
-            xrdm.add_column(Table.Column(name='RDM_SEP', data=[-1.0]))
-            new = join(new, xrdm,keys='INDEX', join_type='outer')
-            new['RDM_ID'].fill_value=-1
-            new['RDM_ID'].mask=True
+                # adding columns related to the crossmatch
+                ccm_tab.add_column(Table.Column(name='INDEX', data=x_id_in_cat))
+                ccm_tab.add_column(Table.Column(name=cat.upper()+'_ID', data=x_id))
+                ccm_tab.add_column(Table.Column(name=cat.upper()+'_SEP', data=x_sep.arcmin))
 
 
-#####WEN
-            
-    if (conf.wen):
+            else:
+                ccm_tab  = Table(masked=True)
+                ccm_tab.add_column(Table.Column(name='INDEX', data=[0]))
+                ccm_tab.add_column(Table.Column(name= cat.upper()+'_ID', data=[-1]))
+                ccm_tab.add_column(Table.Column(name= cat.upper()+'_SEP', data=[np.nan]))
 
-        catwen      = fits.open(conf.wen)
-        ccwen       = catwen[1].data
-        coord_wen = coord.SkyCoord(ccwen['ra'],ccwen['dec'],
-                                    unit=u.deg, frame='fk5')
- 
-        temp_id_wen, temp_sep_wen, trash = pacts1.match_to_catalog_sky(
-                                             coord_wen)
-
-        temp_id_wen  = temp_id_wen.squeeze()
-
-        wheregood = temp_sep_wen.arcmin.squeeze() < conf.wen_dist_theshold_arcmin
-
-        if np.sum(wheregood)>0:
-            if np.sum(wheregood)>1:
-                x_id_wen_in_cat = np.arange(np.size(cgla))[wheregood]
-                x_id_wen        = temp_id_wen[wheregood]
-                x_coord_wen     = coord_wen[x_id_wen]
-                x_sep_wen       = (temp_sep_wen.squeeze())[wheregood]
-            elif np.sum(wheregood)==1:
-                x_id_wen_in_cat = [0]
-                x_id_wen        = [temp_id_wen*1]
-                x_coord_wen     = coord_wen[x_id_wen]
-                x_sep_wen       = temp_sep_wen
-               
-            fields = [str(wen) for wen in conf.wen_fields] # removes u
-            xwen  = Table(ccwen[x_id_wen])           # make cat as Table
-            xwen.keep_columns(fields)                # ewentract the fields
-
-            xwen.add_column(Table.Column(name='INDEX', data=x_id_wen_in_cat))
-            xwen.add_column(Table.Column(name='WEN_ID', data=x_id_wen))
-            xwen.add_column(Table.Column(name='WEN_SEP', data=x_sep_wen.arcmin))
-            
-            for fi in conf.wen_fields:
-                xwen.rename_column(fi, 'WEN_'+fi)
-
-            new = join(new, xwen,keys='INDEX', join_type='outer')
-            new['WEN_ID'].fill_value=-1
-
-
-            if (conf.verbose):
-                print('######################################')
-                print('Results of cross correlation with WEN')
-                print(new)
-
-        else:
-            xwen  = Table(masked=True)
-
-            xwen.add_column(Table.Column(name='INDEX', data=[0]))
-            xwen.add_column(Table.Column(name='WEN_ID', data=[None] ))
-            xwen.add_column(Table.Column(name='WEN_SEP', data=[-1.0]))
-
-            new = join(new, xwen,keys='INDEX', join_type='outer')
-            new['WEN_ID'].fill_value=-1
-            new['WEN_ID'].mask=True
+            match_result[cat.upper()] = ccm_tab
+            if conf.get('outputs', 'verbose'):
+                print('\nResults of cross correlation with '+cat.upper())
+                print(ccm_tab)
 
 
 
-    
-    #print((new['SZDB_ID'].mask)[0], (new['SZDB_ID'].mask)[100])
-    #print((new['SZDB_ID'])[0], (new['SZDB_ID'])[100])
-    
-    
-        
+    # put everything into the output table...
+    for matched_cat in match_result.keys():
+        new = join(new, match_result[matched_cat], keys='INDEX', join_type='outer')
+        new[matched_cat.upper()+'_ID'].fill_value = -1
+
 ###### NED on the not X matched
 ######
 
 ####OUTPUTS TODO global table
 
 
-    
-    filename, extension = os.path.splitext(conf.outputs_filename)
+    outputs_filename = conf.get('outputs','filename')
+    filename, extension = os.path.splitext(outputs_filename)
 
     #print('######################################')
-    #print('File saved in', conf.outputs_filename)
+    #print('File saved in', outputs_filename)
     #print('######################################')
-    
+
     if (extension=='.txt'):
-        ascii.write(new,conf.outputs_filename,
+        ascii.write(new,outputs_filename,
             format='commented_header', fast_writer=False)
     if (extension=='.html'):
-        ascii.write(new,conf.outputs_filename,
+        ascii.write(new,outputs_filename,
             format='html', fast_writer=False)
-        
+
     ## if (extension=='.fits'):
     ##     new.write(conf.outputs_filename,overwrite=True)
 
@@ -354,13 +152,9 @@ def cross_match( file_input,glonglat=[0.,0.],coordframe='galactic'):
     ##                 format='csv', fast_writer=False)
 
 
-    
-
-
-
-
+    # Should return match_cat instead...
     return(new)
-    
+
 
 #if __name__ == "__main__":
 
