cdc1e19f7ff09b792ce531f5bfe2eb43825fdf2b
marian douspis <marian.douspis@ias.u-psud.fr>
2016-02-15 11:19:14 +0100

    better default values

--- views.py
+++ views.py
@@ -15,7 +15,7 @@ class XmatchForm(forms.Form):
 
     COORDFRAME_CHOICE = (('galactic', 'Galactic'), ('fk5', 'Equatorial'))
     coordframe = forms.ChoiceField(widget=forms.RadioSelect, choices=COORDFRAME_CHOICE, initial='galactic')
-    lon = forms.FloatField(label="lon", initial=6.7, min_value=0, max_value=360)
+    lon = forms.FloatField(label="lon", initial=6.76, min_value=0, max_value=360)
     lat = forms.FloatField(label="lat", initial=30.45, min_value=-90, max_value=90)
     pixel_number= forms.FloatField(label="pixel number", initial=256, min_value=0)
     pixel_size= forms.FloatField(label="pixel size (arcmin)", initial=1, min_value=0)
